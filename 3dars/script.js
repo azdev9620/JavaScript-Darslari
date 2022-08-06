17371b7dedfef67a9e006c864e854fef2b2d3294
Azizbek <aziz@mail.com>
2022-08-06 19:22:08 +0500

    3dars
    3dars

---
+++ 3dars/script.js
@@ -0,0 +1,57 @@
+// Function 
+
+
+// ES 5 
+// function sallomBer (name, age = 55) {
+//    console.log('Assalomu aleykum' + name + "  yoshiz  " + age)
+// }
+
+// sallomBer ('Azizbek') 
+
+// const salomBer = () => {   // Fucntion
+//     console.log(`Salom`)
+// }
+// salomBer()
+
+// function calc(a,b) {
+//    return a + b
+// }
+
+// function exp (qwe, zxc) {
+//     var res = 1;
+//     for (let i = 1; i <= zxc; i++) {
+//         res = res * qwe
+//     }
+//     return res
+// }
+
+// var  a = calc(2, 5) 
+
+// console.log(a)
+// var num = +prompt("birinchi raqam yozing")
+// var power = +prompt("Darajasini yozing")
+
+// var res = exp(num, power)// 2, 4
+
+// alert(res);
+
+
+
+// function random (min, max) {
+//     var rand = min + Math.random() * (max - min + 1);
+//     rand = Math.floor(rand)
+//     return rand
+// }
+
+// console.log(random(2, 10));
+
+
+
+var name  = "Azizbek";
+var age = 25;
+
+var city = 'tashkenyt'
+// console.log('Ismim' + name + 'yoshim' + 25)
+
+console.log(`ismim ${name}  yoshim ${age} yashash manzilim ${city}`)
+
